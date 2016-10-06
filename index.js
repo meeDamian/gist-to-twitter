e8dcebba8eb86a33e58675feecdd316bac4472da
Damian Mee <mee.damian@gmail.com>
2016-10-06 22:52:38 +0530

    init pg

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
 
-console.log('init…');
-setTimeout(() => {
-  console.log('did finish!');
-  process.exit(0);
-}, 3000);
+const pg = require('pg-promise')();
+
+const db = pg(process.env.DATABASE_URL);
+
+db.any('SELECT * FROM pipes')
+  .then(data => {
+    data.forEach(x => {
+      console.log(x);
+    });
+
+    console.log('everything did finish!');
+    // process.exit(0);
+  })
+  .catch(err => {
+    console.error('Error:', err);
+    process.exit(1);
+  });
