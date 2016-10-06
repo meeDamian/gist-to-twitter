ed9430c66d27b1d42ed785ac37dd8df20d01bf09
Damian Mee <mee.damian@gmail.com>
2016-10-06 23:19:51 +0530

    promises

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 'use strict';
 
+const https = require('https');
 const pg = require('pg-promise')();
 
 const db = pg(process.env.DATABASE_URL);
 
+function getGistJson({gist}) {
+  return new Promise(resolve => {
+    https.get(`https://api.github.com/gists/${gist}`, res => {
+      const body = '';
+
+      res.on('data', chunk => body += chunk);
+      res.on('end', () => resolve(JSON.parse(body)));
+    }).on('error', err => {
+      console.error(`Error downloading gist: ${gist}`);
+      resolve(null);
+    });
+  });
+}
+
 db.any('SELECT * FROM pipes')
   .then(data => {
-    data.forEach(x => {
-      console.log(x);
-    });
-
-    console.log('everything did finish!');
-    // process.exit(0);
+    return Promise.All(data.map(getGistJson))
+      .then(x => {
+        console.log(x.files);
+      });
   })
+  .then()
+  .then(() => process.exit(0))
   .catch(err => {
     console.error('Error:', err);
     process.exit(1);
